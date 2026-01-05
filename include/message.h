42bf3e255b92d5474b4351c83a3931bf630f5b3a
octavi26 <dimitrie-octavian.radu@s.unibuc.ro>
2026-01-05 12:50:27 +0200

    include protocol change

---
+++ include/message.h
@@ -0,0 +1,28 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#define MAX_CHANNEL 128
+#define MAX_MSG     256
+#define SOCKET_PATH "/tmp/telemetry.socket"
+
+#define TLM_PUBLISHER 0x1
+#define TLM_SUBSCRIBER 0x2
+#define TLM_BOTH 0x3
+
+enum msg_type
+{
+    MSG_SUBSCRIBE,
+    MSG_UNSUBSCRIBE,
+    MSG_PUBLISH,
+    MSG_DELIVER
+};
+
+struct tle_msg
+{
+    enum msg_type type;
+    int client_flags;
+    char channel[MAX_CHANNEL];
+    char payload[MAX_MSG];
+};
+
+#endif
